0737662679c16ec860ab46a4ad5dbc2c798659aa
Ankit Sethi <61274953+4nkitsethi@users.noreply.github.com>
2022-12-29 13:09:56 +0530

    auth changes

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script>
 import BreezeButton from '@/Components/Button.vue';
 import BreezeCheckbox from '@/Components/Checkbox.vue';
 import BreezeGuestLayout from '@/Layouts/Guest.vue';
@@ -7,22 +7,41 @@ import BreezeLabel from '@/Components/Label.vue';
 import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
 import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 
-defineProps({
-    canResetPassword: Boolean,
-    status: String,
-});
-
-const form = useForm({
-    email: '',
-    password: '',
-    remember: false
-});
-
-const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
-};
+export default {
+                    props:["canResetPassword","status"],
+                    data(){
+                                return {    
+                                            form:{
+                                                    credentials : this
+                                                                    .$inertia
+                                                                    .form({ 
+                                                                            email: '',
+                                                                            password: '',
+                                                                            remember: false
+                                                    })
+                                            }
+                                            
+                                }
+                    },
+                    components:{
+                        BreezeButton,
+                        BreezeCheckbox,
+                        BreezeGuestLayout,
+                        BreezeInput,
+                        BreezeLabel,
+                        BreezeValidationErrors,
+                        Head,
+                        Link
+                    },
+                    methods:{
+                        submit(){
+                                    this.form.credentials.post(route('login'), {
+                                        onFinish: () => this.form.credentials.reset('password'),
+                                    });
+                        }
+                    }
+    
+}
 </script>
 
 <template>
@@ -38,17 +57,17 @@ const submit = () => {
         <form @submit.prevent="submit">
             <div>
                 <BreezeLabel for="email" value="Email" class="text-white"/>
-                <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
+                <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.credentials.email" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <BreezeLabel for="password" value="Password"  class="text-white"/>
-                <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
+                <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.credentials.password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label class="flex items-center">
-                    <BreezeCheckbox name="remember" v-model:checked="form.remember"  class="text-white"/>
+                    <BreezeCheckbox name="remember" v-model:checked="form.credentials.remember"  class="text-white"/>
                     <span class="ml-2 text-sm text-gray-600">Remember me</span>
                 </label>
             </div>
